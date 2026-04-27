1bad819699df73ea49b519e255aea133b6fd55f4
kaneeldias <kaneeldias@hotmail.com>
2026-04-27 10:32:33 +0530

    add RequestIdMiddleware to use AWS Lambda request ID; fallback to UUID if not available

--- internal/middleware/request_id.go
+++ internal/middleware/request_id.go
@@ -4,12 +4,20 @@ import (
 	"furniture-search-api/internal/helpers"
 	"net/http"
 
+	"github.com/aws/aws-lambda-go/lambdacontext"
 	"github.com/google/uuid"
 )
 
 func RequestIdMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		requestId := uuid.NewString()
+		var requestId string
+
+		lc, ok := lambdacontext.FromContext(r.Context())
+		if ok {
+			requestId = lc.AwsRequestID
+		} else {
+			requestId = uuid.New().String()
+		}
 
 		r.Header.Set(helpers.RequestIDHeader, requestId)
 		r = r.WithContext(helpers.WithRequestId(r.Context(), requestId))
